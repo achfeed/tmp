8670df4b6725cb29ad526776b428811c0b048737
Achraf Hamid <72417895+achfeed@users.noreply.github.com>
2022-02-07 18:30:41 +0100

    Update Parse ics file.py

--- Parse ics file.py
+++ Parse ics file.py
@@ -3,14 +3,15 @@ in_file = "C:/Users/ahamid/Downloads/tiesto.ics"
 from icalendar import Calendar, Event    # icalendar.__version__ == 4.0.9
 g = open(in_file,'rb')
 gcal = Calendar.from_ical(g.read())
-title = ''
-start_time = ''
-end_time = ''
+title = ''   # ici sera stocké le titre que l'événement google calendar porte (ex : Daily)
+start_time = '' # Date et heure de début (à besoin d'être parsée, je le ferais et modifierais ici)
+end_time = '' # Date et heure de fin (à besoin d'être parsée, je le ferais et modifierais ici)
 for component in gcal.walk():
     if component.name == "VEVENT":
         title = component.get('summary')
         start_time = component.get('dtstart').to_ical().decode("utf-8") 
         end_time = component.get('dtend').to_ical().decode("utf-8") 
+        # d'autres éléments peuvent être récupérés mais ne sont pas toujours présents dans un fichier ics :
         try:
             print('--- ATTENDEE : \n', component.get('ATTENDEE'))
         except:
